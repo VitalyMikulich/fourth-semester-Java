a8e1b7edc074ea76eed937741153f17bb442d8fb
VitalyMikulich <mr.wital1999@gmail.com>
2019-03-15 09:19:13 +0300

    fix lab3c

--- Lab3c/src/by/bsu/lab3c/Main.java
+++ Lab3c/src/by/bsu/lab3c/Main.java
@@ -26,6 +26,12 @@ public class Main {
         System.out.println("Что вы хотите отобразить: 1 - X; 2 - Y");
         int choice = in.nextInt();
         indexing(vectors, choice, m);
+        System.out.println("Угол между векторами: " + "X: " + vectors[0].getX() + ", Y: " + vectors[0].getY() + " и "  + "X: " + vectors[1].getX() + ", Y: " + vectors[1].getY()  + " равен " + angle(vectors[0], vectors[1]));;
+        angle(vectors[0], vectors[1]);
+        System.out.println("Угол между векторами: " + "X: " + vectors[1].getX() + ", Y: " + vectors[1].getY() + " и "  + "X: " + vectors[2].getX() + ", Y: " + vectors[2].getY() + " равен " + angle(vectors[1], vectors[2]));
+        angle(vectors[1], vectors[2]);
+        System.out.println("Угол между векторами: " + "X: " + vectors[0].getX() + ", Y: " + vectors[0].getY() + " и "  + "X: " + vectors[2].getX() + ", Y: " + vectors[2].getY() + " равен " + angle(vectors[0], vectors[2]));
+        angle(vectors[0], vectors[2]);
     }
 
     public static void increment (Vector[] vectors, int m){
@@ -55,5 +61,17 @@ public class Main {
                 System.out.println("Координата Y " + (i+1) + "-ого вектора - " + vectors[i].getY());
             }
         }
+    }
+
+    public static double lenght_vect (Vector vector){
+        return Math.sqrt(Math.pow(vector.getX(), 2) + Math.pow(vector.getY(), 2));
+    }
+
+    public static double scalar_product (Vector vector1, Vector vector2){
+        return vector1.getX()*vector2.getX() + vector1.getY()*vector2.getY();
+    }
+
+    public static double angle (Vector vector1, Vector vector2){
+        return Math.acos(scalar_product(vector1, vector2)/(lenght_vect(vector1)*lenght_vect(vector2)));
     }
 }
